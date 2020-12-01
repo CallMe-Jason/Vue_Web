1993e6e7d5d13ab4c4dcf7ea72eb261fac7df850
CallMe-Jason <2872213719@qq.com>
2020-12-01 20:03:12 +0800

    未完成商品列表

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 import axios from 'axios'
 // import { config } from 'vue/types/umd'
+import TreeTable from 'vue-table-with-tree-grid'
 
 axios.interceptors.request.use(config=>{
   // console.log(config);
@@ -17,6 +18,8 @@ axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+Vue.component('tree-table',TreeTable)
 
 new Vue({
   router,
